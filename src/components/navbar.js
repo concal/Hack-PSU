e5b72e6e7b4639cb4cb4c9d3dcf3f887beb2b121
Connor Calderon <cpc329@gmail.com>
2022-04-09 15:47:36 -0400

    Add react boostrap

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React from 'react';
 import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+
+const isLoggedIn = localStorage.getItem('user');
 
 const Navbar = () => {
     return (
         <div className="navbar-container">
-            <Link to="./" className="navbar-logo-link">Project Name</Link>
+            <Link to="./" className="navbar-logo-link">
+                Give me a Nintendo Switch :)
+            </Link>
             <div className="navbar-links">
-                <Link to="./" className="navbar-link">Home</Link>
+                <Link to="./" className="navbar-link">
+                    Home
+                </Link>
+                {isLoggedIn ? (
+                    <Button variant="success">Log out</Button>
+                ) : (
+                    <Button variant="success">Log in</Button>
+                )}
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Navbar;
