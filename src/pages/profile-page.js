96fa8915780d1cc0b3407a8ee6e065354760fb5f
Connor Calderon <cpc329@gmail.com>
2022-04-10 09:15:31 -0400

    move images back

--- src/pages/profile-page.js
+++ src/pages/profile-page.js
@@ -1,28 +1,40 @@
 import React from 'react';
 import Card from '../components/card';
-import testimage from '.../public/testimages/TESTUSER.jpg'
-
+import testimage from '../testimages/TESTUSER.jpg';
 
 const ProfilePage = () => {
-//api call
-// response
+    //api call
+    // response
 
-  return (
-    <div className="profile-page-container">
-      <div className="profile-flex-box-container">
-        <div className="profile-card-container">
-          <img className="profile-photo"src={testimage} alt="userimage"/>
-          <b className="profile-name"> CLUB NAME</b>
-          <p className="profile-description"align="left">club description</p> {/*LIMIT THE LENGTH OF THIS DESCRIPTION */}
+    return (
+        <div className="profile-page-container">
+            <div className="profile-flex-box-container">
+                <div className="profile-card-container">
+                    <img
+                        className="profile-photo"
+                        src={testimage}
+                        alt="userimage"
+                    />
+                    <b className="profile-name"> CLUB NAME</b>
+                    <p className="profile-description" align="left">
+                        club description
+                    </p>{' '}
+                    {/*LIMIT THE LENGTH OF THIS DESCRIPTION */}
+                </div>
+                <div className="profile-events-container">
+                    <Card
+                        title={'hi'}
+                        clubName={'hi'}
+                        description={'hi'}
+                        location={'hi'}
+                        startTime={'hi'}
+                        endTime={'hi'}
+                        style={{}}
+                    />
+                </div>
+            </div>
         </div>
-        <div className="profile-events-container">
-          <Card title={"hi"} clubName={"hi"} description={"hi"} location={"hi"} startTime={"hi"} endTime={"hi"} style={{}}/>
-          
-        
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ProfilePage;
